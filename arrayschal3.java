c1ee6fc7946c0285e0963d64ca9c5255aa4f3420
audreyho <aallerina@gmail.com>
2015-07-04 03:45:43 +0800

    More week 2 hwk

---
+++ arrayschal3.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class arrayschal3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arrayy[];
+		arrayy = new String [3];
+		arrayy[0]=" This is the";
+		arrayy[1]="For all the readers from";
+		arrayy[2]="Acadgild";
+		
+		System.out.println(Arrays.toString(arrayy));
+
+	}
+
+}
---
+++ reversearrayy.java
@@ -0,0 +1,24 @@
+
+public class reversearrayy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int reversearray[];
+		reversearray = new int [5];
+		reversearray[0] = 99;
+		reversearray[1] = 88;
+		reversearray[2] = 77;
+		reversearray[3] = 66;
+		reversearray[4] = 55;
+
+		
+		for(int i= 4; i>=0 ; i--){
+			System.out.println(reversearray[i]);
+		}
+		
+		
+		
+
+	}
+
+}
